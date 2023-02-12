f44b4f52490093826548e9316fd745340a8f1366
Shrohan Mohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2023-02-11 23:10:37 -0500

    Add files via upload

--- sandbox1.py
+++ sandbox1.py
@@ -82,6 +82,25 @@ print('where the compatibility conditions are given by')
 for equation in compatibility:
 	if equation != 0: print('>',Eq(equation, 0))
 
+PowerLawNonlinearKG = parse_expr('diff(f(x,t),x,t)-f(x,t)**4')
+print()
+print(
+	'Let\'s check the Backlund transform from the '+\
+	'Painleve property of the power law nonlinear Klein-Gordon'
+	)
+alpha, transform, compatibility = ultimatePainleve(
+		PowerLawNonlinearKG, # Input PDE
+		x, # The input variable used for the spatial variable
+		t # The input variable used for the temporal variable
+		# alpha # The power balancing exponent
+	)
+print()
+print('The power balancing exponent is ',alpha)
+print('The transform is given by U =',  transform)
+print('where the compatibility conditions are given by')
+for equation in compatibility:
+	if equation != 0: print('>',Eq(equation, 0))
+
 CaudreyDoddGibbonPDE = parse_expr('diff(f(x, t), t)+diff('+
 	'diff(f(x, t), x, x, x, x) + 30*f(x, t)*diff(f(x, t), x)'+
 	' + 60*f(x, t)**3, x)')
@@ -126,53 +145,52 @@ for equation in compatibility:
 print()
 
 
-b = 2
-CamassaHolmEquation = parse_expr('2*f(x,t)**3*diff(f(x,t),x)'
-	+'+diff(f(x,t),t)*(diff(f(x,t),x)**2-f(x,t)*diff(f(x,t),x,x)-1)'
-	+'+f(x,t)*(f(x,t)*diff(f(x,t),x,x,t)-diff(f(x,t),x)*diff(f(x,t),x,t))'
-	)
-print()
-print(
-	'Let\'s check the Backlund transform from the '+
-	'Painleve property of the Camassa-Holm equation'
-	)
-alpha, transform, compatibility = ultimatePainleve(
-		CamassaHolmEquation, # Input PDE
-		x, # The input variable used for the spatial variable
-		t, # The input variable used for the temporal variable
-		# alpha # The power balancing exponent
-	)
-print()
-print('The power balancing exponent is ',alpha)
-print('The transform is given by U = ',  transform)
-print('where the compatibility conditions are given by')
-for equation in compatibility:
-	if equation != 0: print('>',Eq(equation, 0))
+# b = 2
+# CamassaHolmEquation = parse_expr('2*f(x,t)**3*diff(f(x,t),x)'
+# 	+'+diff(f(x,t),t)*(diff(f(x,t),x)**2-f(x,t)*diff(f(x,t),x,x)-1)'
+# 	+'+f(x,t)*(f(x,t)*diff(f(x,t),x,x,t)-diff(f(x,t),x)*diff(f(x,t),x,t))'
+# 	)
+# print()
+# print(
+# 	'Let\'s check the Backlund transform from the '+
+# 	'Painleve property of the Camassa-Holm equation'
+# 	)
+# alpha, transform, compatibility = ultimatePainleve(
+# 		CamassaHolmEquation, # Input PDE
+# 		x, # The input variable used for the spatial variable
+# 		t, # The input variable used for the temporal variable
+# 		# alpha # The power balancing exponent
+# 	)
+# print()
+# print('The power balancing exponent is ',alpha)
+# print('The transform is given by U = ',  transform)
+# print('where the compatibility conditions are given by')
+# for equation in compatibility:
+# 	if equation != 0: print('>',Eq(equation, 0))
 
 
-b = 3
-DegasperisProcesiEquation = parse_expr('3*f(x,t)**4*diff(f(x,t),x)'
-	+'+diff(f(x,t),t)*(diff(f(x,t),x)**2-f(x,t)*diff(f(x,t),x,x)-1)'
-	+'+f(x,t)*(f(x,t)*diff(f(x,t),x,x,t)-diff(f(x,t),x)*diff(f(x,t),x,t))'
-	)
-print()
-print(
-	'Let\'s check the Backlund transform from the '+
-	'Painleve property of the Degasperis-Procesi equation'
-	)
-alpha, transform, compatibility = ultimatePainleve(
-		DegasperisProcesiEquation, # Input PDE
-		x, # The input variable used for the spatial variable
-		t, # The input variable used for the temporal variable
-		# alpha # The power balancing exponent
-	)
-print()
-print('The power balancing exponent is ',alpha)
-print('The transform is given by U = ',  transform)
-print('where the compatibility conditions are given by')
-for equation in compatibility:
-	if equation != 0: print('>',Eq(equation, 0))
-
+# b = 3
+# DegasperisProcesiEquation = parse_expr('3*f(x,t)**4*diff(f(x,t),x)'
+# 	+'+diff(f(x,t),t)*(diff(f(x,t),x)**2-f(x,t)*diff(f(x,t),x,x)-1)'
+# 	+'+f(x,t)*(f(x,t)*diff(f(x,t),x,x,t)-diff(f(x,t),x)*diff(f(x,t),x,t))'
+# 	)
+# print()
+# print(
+# 	'Let\'s check the Backlund transform from the '+
+# 	'Painleve property of the Degasperis-Procesi equation'
+# 	)
+# alpha, transform, compatibility = ultimatePainleve(
+# 		DegasperisProcesiEquation, # Input PDE
+# 		x, # The input variable used for the spatial variable
+# 		t, # The input variable used for the temporal variable
+# 		# alpha # The power balancing exponent
+# 	)
+# print()
+# print('The power balancing exponent is ',alpha)
+# print('The transform is given by U = ',  transform)
+# print('where the compatibility conditions are given by')
+# for equation in compatibility:
+# 	if equation != 0: print('>',Eq(equation, 0))
 
 # b = 2.5
 # alpha = -2
